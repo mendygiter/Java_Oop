3ef83ea4af4cf805371fe2db0558a32e576cf5d3
mendygiter <m.giter@fusegen.ai>
2022-06-10 17:51:07 -0400

    first commit

---
+++ caresoft/src/main/java/com/codingdojo/CaresoftApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaresoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaresoftApplication.class, args);
+	}
+
+}
